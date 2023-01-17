ec4d6a993d47e0388a813f1635622bd6306fb49c
김진훈\Owner <kjh1833000@naver.com>
2023-01-17 23:04:58 +0900

    [진훈] 프로젝트 초기 세팅

---
+++ Assets/Scripts/Camera/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Camera
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        private List<Transform> _tr;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            foreach (var tr in _tr.Where(tr => Math.Abs(tr.position.x - 1.0f) < 3.0f))
+            {
+                Debug.Log(tr.position);
+            }
+        }
+
+        /// <summary>
+        /// [진훈] 아이템을 판매하는 함수 입니다.
+        /// </summary>
+        /// <param name="itemID">판매하려는 아이템의 아이디 입니다.</param>
+        public void Sell(int itemID)
+        {
+        
+        }
+
+    
+    }
+}
